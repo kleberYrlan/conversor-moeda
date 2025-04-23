b7bc7190baf7f3e95ea288fcb028dc15d1dcb7d1
kleberYrlan <estudodiscord@gmail.com>
2025-04-23 18:42:23 -0300

    Add files via upload

---
+++ src/Consulta.java
@@ -0,0 +1,33 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Consulta {
+    private final String baseCoin;
+    private final String targetCoin;
+
+    public Consulta(String baseCoin, String targetCoin) {
+        this.baseCoin = baseCoin;
+        this.targetCoin = targetCoin;
+    }
+
+    public Convertido ConversaoMoeda(double amount) throws IOException, InterruptedException {
+        String key = "77300dc0bbe19730444b7e1a";
+        String endereco = "https://v6.exchangerate-api.com/v6/" + key + "/pair/" + this.baseCoin + "/" +
+                targetCoin + "/" + amount;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        Gson gson = new Gson();
+
+        return gson.fromJson(response.body(), Convertido.class);
+    }
+}
